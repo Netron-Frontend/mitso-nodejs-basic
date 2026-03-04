c2e3c5fcb1d9d341e63a5eedbbf71cb34813b4a0
student <ivanmakarevic3@gmail.com>
2026-03-04 15:46:12 +0300

    feat: end work with streams folder

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            const str = chunk.toString();
+            const reversed = str.split('').reverse().join('');
+            callback(null, reversed);
+        }
+    });
+
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
+
+    console.log('Введите текст для переворота:');
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
-const write = async () => {
-    // Write your code here 
+import fs from 'fs';
+
+const read = async () => {
+
+    const writeStream = fs.createWriteStream('./files/fileToWrite.txt');
+
+    process.stdin.pipe(writeStream);
+
+    writeStream.on('error', (error) => {
+        console.error('Ошибка при записи в файл:', error.message);
+    });
 };
 
-await write();
+await read();
