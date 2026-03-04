2e269ba681fa5077b4caddb40255c3df9818e588
student <ivanmakarevic3@gmail.com>
2026-03-04 15:10:34 +0300

    feat: end work with cli folder

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,6 +1,16 @@
 const parseEnv = () => {
-    process.env.MITSO_NAME = value1;
-    MITSO_NAME_2 = value2;
+    process.env.MITSO_name1='value1';
+    process.env.MITSO_name2='value2';
+
+    const result = [];
+
+    for (const [key, value] of Object.entries(process.env)) {
+        if (key.startsWith('MITSO_')) {
+            result.push(`${key}=${value}`);
+        }
+    }
+
+    console.log(result.join('; '));
 };
 
 parseEnv();
