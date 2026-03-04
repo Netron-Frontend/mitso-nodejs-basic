7a05636c46f33258a62024ef90801f35856696dc
student <ivanmakarevic3@gmail.com>
2026-03-04 14:52:39 +0300

    feat: end work with fs library

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,11 +1,5 @@
-import fs from 'fs'
-import * as path from "node:path";
-
 const parseArgs = () => {
-
-    const data = fs.readFileSync('env'), 'utf8');
-
-    console.log(`--propName` data)
+    // Write your code here
 };
 
 parseArgs();
